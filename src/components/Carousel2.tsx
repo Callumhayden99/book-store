1f0669f2391c8c7cd6c73a52c348eac83c24ef99
Jack Miller <127975048+jdm1991@users.noreply.github.com>
2024-04-01 15:28:40 +0100

    Carousel 2 images updated

--- src/components/Carousel2.tsx
+++ src/components/Carousel2.tsx
@@ -1,28 +1,28 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
+
 import React, { useState } from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 const Carousel2 = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-
   const images = [
-    "/images/book1.jpg",
-    "/images/book2.jpg",
-    "/images/book3.jpg",
-    "/images/book4.jpg",
-    "/images/book5.jpg",
-    "/images/book6.jpg",
-    "/images/book7.jpg",
-    "/images/book8.jpg",
-    "/images/book9.jpg",
-    "/images/book10.jpg",
-    "/images/book11.jpg",
-    "/images/book12.jpg",
+    "/CasinoRoyale.jpg",
+    "/churchill.jpg",
+    "/DrNo.jpg",
+    "/DuneBook.jpeg",
+    "/FromRussiaWithLove.jpeg",
+    "/iAmPilgrim.jpg",
+    "/normalPeople.jpg",
+    "/oxygenAdvantageBook.jpeg",
+    "/redSparrow.jpg",
+    "/warandpeaceBook.jpg",
   ];
 
   const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
   };
 
   const nextSlide = () => {
