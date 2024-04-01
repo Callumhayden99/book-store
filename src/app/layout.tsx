c62da2efc4438b19b202cab0f61e2151cf7e0ec9
callumhayden <callum.hayden@gmail.com>
2024-04-01 09:23:46 +0100

    fix: files
    updated the folder and files names to get my pages showing on the application
    
    ref: home, about

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,25 +32,27 @@ export default function RootLayout({
       <body className={inter.className}>
         <header className="bg-white shadow-md">
           <div className="container mx-auto px-4 py-6">
-            <div className="flex flex-col md:flex-row items-center justify-between">
+            <div className="flex flex-wrap justify-between items-center">
               <div className="flex items-center mb-4 md:mb-0">
                 <Link href="/">
-                  <span className="text-xl font-bold text-red-600">BookBank</span>
+                  <span className="text-xl font-bold text-red-600">
+                    BookBank
+                  </span>
                 </Link>
               </div>
-              <div className="flex-1 flex justify-center mb-4 md:mb-0">
+              <div className="w-full md:w-auto mb-4 md:mb-0 md:mr-5 md:ml-auto">
                 <div className="relative">
                   <input
                     type="text"
                     placeholder="Search books..."
-                    className="w-full md:w-96 px-4 py-2 pl-10 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-600"
+                    className="w-full px-4 py-2 pl-10 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-red-600"
                   />
                   <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
                 </div>
               </div>
               <div className="flex items-center space-x-4">
                 <nav className="hidden md:block">
-                  <ul className="flex space-x-4">
+                  <ul className="flex flex-wrap justify-center space-x-4">
                     <li>
                       <Link href="/home">
                         <span className="text-gray-800 hover:text-red-600 flex items-center">
@@ -92,7 +94,7 @@ export default function RootLayout({
               </div>
             </div>
             <nav className="md:hidden mt-4">
-              <ul className="flex space-x-4 justify-center">
+              <ul className="flex flex-wrap justify-center space-x-4">
                 <li>
                   <Link href="/home">
                     <span className="text-gray-800 hover:text-red-600 flex items-center">
