6bc0ae6d3ef813aeda38b2e8077d6a6cdf4bdbab
callumhayden <callum.hayden@gmail.com>
2024-04-04 18:58:10 +0100

    feat:biograpgy books added
    created the end point and get request to receive the biography books
    
    ref: bio.js, biography/page.tsx

---
+++ src/pages/api/bio.js
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    try {
+      const fictionBooks = await prisma.book.findMany({
+        where: {
+          genre: "Biography",
+        },
+      });
+
+      res.status(200).json(fictionBooks);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
