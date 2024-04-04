89377c1e23b0ab41df16d7dce3710831eb251556
callumhayden <callum.hayden@gmail.com>
2024-04-04 18:54:38 +0100

    feat: created classic page
    created the classic page and also have the end point created and get request working to receive the classic books
    
    ref: classic.js, classic/page.tsx

--- src/app/mystery/page.tsx
+++ src/app/mystery/page.tsx
@@ -61,7 +61,7 @@ export default function Mystery() {
     <>
       <div className="container mx-auto py-8">
         <h1 className="text-4xl font-bold mb-8 text-center text-gray-800">
-          Fiction Books
+          Mystery Books
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {books.map((book) => (
--- src/app/non-fiction/page.tsx
+++ src/app/non-fiction/page.tsx
@@ -61,7 +61,7 @@ export default function NonFiction() {
     <>
       <div className="container mx-auto py-8">
         <h1 className="text-4xl font-bold mb-8 text-center text-gray-800">
-          Fiction Books
+          Non-Fiction Books
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {books.map((book) => (
